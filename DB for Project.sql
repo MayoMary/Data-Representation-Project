77a35c189bc97ca6df31761aed7d05889920eccc
MayoMary <36369533+MayoMary@users.noreply.github.com>
2020-01-06 20:07:05 +0000

    Add files via upload
    Updated to include the items

--- DB for Project.sql
+++ DB for Project.sql
@@ -1,4 +1,4 @@
-#creating database to use in Project
+#Creating database to use in Project
 
 create table handbag (
 	id int NOT NULL auto_increment,
@@ -8,3 +8,7 @@ create table handbag (
 	PRIMARY KEY(id)
 );
 
+insert into handbag (designer, handbag,price) values ('Louis Vuitton','Alma bag', 1025); 
+insert into handbag (designer, handbag,price) values ('Gucci','Quilted shoulder bag', 10000); 
+insert into handbag (designer, handbag,price) values ('Balenciaga','City bag', 2200); 
+insert into handbag (designer, handbag,price) values ('Dior','Sadle bag', 2800); 
